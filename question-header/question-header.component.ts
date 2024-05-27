3aa4598c9c16ec0d68a3e9c0583dc9bc3bed5e70
Sarah Schlegel <sschlegel@myges.fr>
2024-05-27 14:59:29 +0200

    refacto landing

--- question-header/question-header.component.ts
+++ question-header/question-header.component.ts
@@ -7,11 +7,13 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 })
 export class QuestionHeaderComponent {
   @Input() id: number = 1;
-  @Input() points: number = 0.5;
+  @Input() points: number = 1.0;
+  @Input() edit: boolean = false;
   @Output() changeEvent = new EventEmitter<number>;
-  value: number = 0.5;
+  value: number = 1;
 
   change() {
+    console.log(this.value);
     this.changeEvent.emit(this.value)
   }
 }
