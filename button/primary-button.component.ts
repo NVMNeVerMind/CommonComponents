54e32ccfad9d854b8c35d3a646dbe92164859d04
Nathan-dev-dot <maerna1@gmail.com>
2024-06-26 17:51:46 +0200

    add feature into app-button

--- button/primary-button.component.ts
+++ button/primary-button.component.ts
@@ -7,12 +7,14 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 })
 export class PrimaryButtonComponent {
   @Input() label: string = '';
+  @Input() rounded: boolean = false;
   @Input() primary: boolean = true;
   @Input() styleClass: string = '';
   @Input() disabled: boolean = false;
 
   @Output() clickEvent: EventEmitter<boolean> = new EventEmitter<boolean>()
   @Input() bigButton: boolean  = false;
+  @Input() fixed: boolean = false;
   emit() {
     this.clickEvent.emit(true)
   }
