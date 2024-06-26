03b5b4076d84deba62681bd6aa3b57e2aa0f1345
Nathan-dev-dot <maerna1@gmail.com>
2024-06-26 14:17:50 +0200

    modify image input

--- image-input/image-input.component.ts
+++ image-input/image-input.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-image-input',
@@ -8,7 +8,7 @@ import {Component, EventEmitter, Output} from '@angular/core';
 export class ImageInputComponent {
 
   @Output() change: EventEmitter<File> = new EventEmitter();
-
+  @Input() id: string = '';
   readonly defaultLabel = 'Choisir une image';
   fileName: null | string = null;
   fileNamePreview = ''
