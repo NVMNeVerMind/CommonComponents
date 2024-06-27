5634db867130941438f884912e25ebf1d6cc4869
Sarah Schlegel <sschlegel@myges.fr>
2024-06-27 11:51:42 +0200

    header button

---
+++ button-header/button-header.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-button-header',
+  templateUrl: './button-header.component.html',
+  styleUrls: ['./button-header.component.scss']
+})
+export class ButtonHeaderComponent {
+  @Input() value: string = "";
+}
