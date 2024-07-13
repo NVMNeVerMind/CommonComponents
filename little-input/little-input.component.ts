9de43909a7a4d38b6e6393d9ea2aa5ae386df38e
ohmushi <omnes.theo@gmail.com>
2024-07-13 19:20:13 +0200

    fix timer change

--- little-input/little-input.component.ts
+++ little-input/little-input.component.ts
@@ -34,4 +34,13 @@ export class LittleInputComponent {
   }
 
   protected readonly Theme = Theme;
+
+  getDefault() {
+    if(this.selectedOption.length === 1) {
+      this.change.emit(this.selectedOption[0].id);
+      return "selected";
+    } else {
+      return null;
+    }
+  }
 }
