beb46a8458f5d433a667c29449d133988a8b4976
Nathan-dev-dot <maerna1@gmail.com>
2024-07-17 11:47:54 +0200

    fix select

--- little-input/little-input.component.ts
+++ little-input/little-input.component.ts
@@ -35,12 +35,13 @@ export class LittleInputComponent {
 
   protected readonly Theme = Theme;
 
-  getDefault() {
-    if(this.selectedOption.length === 1) {
-      this.change.emit(this.selectedOption[0].id);
-      return "selected";
-    } else {
-      return null;
-    }
-  }
+  // getDefault() {
+  //   console.log("Coucou");
+  //   if(this.selectedOption.length === 1) {
+  //     this.change.emit(this.selectedOption[0].id);
+  //     return "selected";
+  //   } else {
+  //     return null;
+  //   }
+  // }
 }
