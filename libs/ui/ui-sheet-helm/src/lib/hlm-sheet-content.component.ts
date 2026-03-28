f19f0d466f8d31a11e453cece6ba641c940a4d18
Sarah Schlegel <grepo.sarah19@gmail.com>
2026-03-28 12:48:15 +0100

    update components

--- libs/ui/ui-sheet-helm/src/lib/hlm-sheet-content.component.ts
+++ libs/ui/ui-sheet-helm/src/lib/hlm-sheet-content.component.ts
@@ -10,7 +10,7 @@ import type {ClassValue} from 'clsx';
 import {HlmSheetCloseDirective} from './hlm-sheet-close.directive';
 
 export const sheetVariants = cva(
-	'fixed !max-w-none !w-fit z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500',
+	'fixed !max-w-none !w-fit z-50 gap-4 bg-primary p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500',
 	{
 		variants: {
 			side: {
@@ -38,7 +38,7 @@ export const sheetVariants = cva(
     },
     template: `
 		<ng-content />
-		<button brnSheetClose hlm>
+		<button brnSheetClose hlm class="text-primary-foreground">
 			<span class="sr-only">Close</span>
 			<ng-icon hlm class="flex h-4 w-4" name="lucideX" />
 		</button>
