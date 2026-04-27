373f546d04a7fe15e2b0a386a57f42ed85625c8a
Sarah Schlegel <grepo.sarah19@gmail.com>
2026-04-27 13:08:38 +0200

    searchable select & reset/renew password btn style

--- components-list.ts
+++ components-list.ts
@@ -28,6 +28,7 @@ import {SwitchComponent} from "./switch/switch.component";
 import {UserMenuButtonComponent} from "./user-menu-button/user-menu-button.component";
 import {ProfileComponent} from "./profile/profile.component";
 import {ClassDropdownComponent} from "./class-dropdown/class-dropdown.component";
+import {SearchableSelectComponent} from "./searchable-select/searchable-select.component";
 
 export const ComponentsList = [
     PrimaryButtonComponent,
@@ -59,5 +60,6 @@ export const ComponentsList = [
     SwitchComponent,
     UserMenuButtonComponent,
     ProfileComponent,
-  ClassDropdownComponent
+  ClassDropdownComponent,
+  SearchableSelectComponent,
 ]
